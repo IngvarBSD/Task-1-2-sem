b240b85391deb805d38d50551d7e2a57444af200
KandaurovIgn <72686706+KandaurovIgn@users.noreply.github.com>
2021-02-24 18:22:47 +0300

    Delete Task1GUI directory

--- Task1GUI/src/ru/vsu/cs/common/CommonCredit.java
+++
@@ -1,6 +0,0 @@
-package ru.vsu.cs.common;
-
-public abstract class CommonCredit {
-    public abstract double getSumOfPayForNMonth(int nMonth, double sum, int deadline, double annualInterestRate);
-    public abstract double getTotalSumOfPay(double sum, int deadline, double annualInterestRate);
-}
